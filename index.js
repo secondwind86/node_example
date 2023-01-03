39b9cdc928afebce3de76c30130ac3aee6d7da67
secondwind86 <53468147+secondwind86@users.noreply.github.com>
2023-01-03 21:34:15 +0900

    비밀 정보 보호 처리 dev/prd

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const { User } = require("./models/User")
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+// application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// application/json
+app.use(bodyParser.json())
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb+srv://kyo:137lkh@kcluster.ydwswyk.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('hello World~ 안녕하세요'))
+app.get('/', (req, res) => res.send('hello World~ 안녕하세요 새해 복 많이 받으세요 캬캬캬캬'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err, userInfo) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
